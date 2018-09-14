cc8a95b0e55eabb17792b3195c4dc5a1d3d6a640
hoparkgo9ma <me@ho9.me>
2018-09-14 23:18:20 +0900

    update expired comment > timer.js

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("sat").innerHTML = "EXPIRED";
+    document.getElementById("sat").innerHTML = "이미 너는 최고야 :)";
   }
 }, 1000);
