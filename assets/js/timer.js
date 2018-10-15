859a2f45be066b473927c71afe2bbaa0ba11754c
hoparkgo9ma <me@ho9.me>
2018-10-15 12:54:52 +0900

    Update timer.js

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,26 +1,19 @@
-// Set the date we're counting down to
 var countDownDate = new Date("Nov 15, 2018 08:40:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get todays date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("sat").innerHTML = "수능 시작까지 " + days + "일 " + hours + "시간 "
   + minutes + "분 " + seconds + "초. ";
 
-  // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("sat").innerHTML = "이미 너는 최고야 :)";
